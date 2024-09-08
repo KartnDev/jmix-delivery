2cb8557ec8da523f4e051556975f8fedaff33bd6
kartondev <d.cherkasov@haulmont.com>
2024-09-09 02:13:35 +0300

    Step 2.5: Add path constants as pre-step on OrderView

---
+++ src/main/java/io/jmix/delivery/constants/OrderViewsPathConstants.java
@@ -0,0 +1,7 @@
+package io.jmix.delivery.constants;
+
+public final class OrderViewsPathConstants {
+    public static final String NEW_ORDER_ID = "new";
+    public static final String RESTAURANT_ID_PATH_PARAM = "restaurantId";
+    public static final String ORDER_ID_PATH_PARAM = "id";
+}
