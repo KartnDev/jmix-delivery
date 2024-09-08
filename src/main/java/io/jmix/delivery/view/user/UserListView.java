07a4e1454aec353a7c6bfb8c5953766c0636f73e
kartondev <d.cherkasov@haulmont.com>
2024-09-08 18:42:04 +0300

    init commit

---
+++ src/main/java/io/jmix/delivery/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package io.jmix.delivery.view.user;
+
+import io.jmix.delivery.entity.User;
+import io.jmix.delivery.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
