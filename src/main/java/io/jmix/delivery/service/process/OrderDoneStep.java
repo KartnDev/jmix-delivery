b7e8d844a7a92cd439a513fb93124e8c5217e26e
kartondev <d.cherkasov@haulmont.com>
2024-09-13 17:59:18 +0300

    Step 4.4: Fully implemented whole order process via ServiceTasks steps

---
+++ src/main/java/io/jmix/delivery/service/process/OrderDoneStep.java
@@ -0,0 +1,34 @@
+package io.jmix.delivery.service.process;
+
+import io.jmix.core.DataManager;
+import io.jmix.core.FetchPlans;
+import io.jmix.core.SaveContext;
+import io.jmix.core.security.SystemAuthenticator;
+import io.jmix.delivery.entity.Order;
+import io.jmix.delivery.entity.OrderStatus;
+import io.jmix.delivery.repository.OrderRepository;
+import io.jmix.delivery.service.process.abc.AbstractTransactionalStep;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Component(value = "orderDoneStep")
+public class OrderDoneStep extends AbstractTransactionalStep {
+
+    protected OrderDoneStep(SystemAuthenticator systemAuthenticator,
+                            RuntimeService runtimeService,
+                            PlatformTransactionManager transactionManager,
+                            DataManager dataManager,
+                            OrderRepository orderRepository,
+                            FetchPlans fetchPlans) {
+        super(systemAuthenticator, runtimeService, transactionManager, dataManager, orderRepository, fetchPlans);
+    }
+
+    @Override
+    protected void doTransactionalStep(DelegateExecution execution, Order order, SaveContext saveContext) {
+        order.setStatus(OrderStatus.READY);
+        doSomeWork();
+        saveContext.saving(order);
+    }
+}
